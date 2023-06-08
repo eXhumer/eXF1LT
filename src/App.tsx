80b271359bdc1fcee12f67f551dff3d300ad7e2b
eXhumer <exhumer1@protonmail.com>
2023-06-07 18:02:45 -0600

    chore: Add license boilerplate to source

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+// eXF1LT - Live Timing client for Formula 1
+// Copyright (C) 2023  eXhumer
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, version 3 of the License.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { useEffect, useState } from "react";
 import { Box } from "ink";
 import { F1LiveTimingClient } from "./Client.js";
